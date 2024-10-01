69fedd98f079dd9735d26effdad8d8ff57fa512b
gilfoyle <tanmay.barjatya10@gmail.com>
2024-10-01 21:38:25 +0530

    code refactored

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const IMAGE_URL =
+  "https://img.freepik.com/free-vector/womans-hand-putting-dollar-bill-into-open-mans-wallet-payment-transfer-money-person-holding-purse-flat-vector-illustration-finance-concept-banner-website-design-landing-web-page_74855-25048.jpg?w=740&t=st=1727791333~exp=1727791933~hmac=f65140ead6868acd901a6df0ac2bae288635d4b67a231fab22ffd8d4ad93eaca";
